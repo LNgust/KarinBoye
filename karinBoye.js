4d220b6e7c1b0c101d2c6b75c686410183eadb30
Beep Boop <0302elgu@gapp.uddevalla.se>
2021-12-15 13:33:47 +0100

    created new html files
    created new html files for each work by karin boye

--- karinBoye.js
+++ karinBoye.js
@@ -1,3 +1,21 @@
-document.getElementById("btnAstarte").onclick = function () {
-    location.href = "astarte.html";
-};
+function addLiVerk() {
+    const node = document.createElement("LI");
+    const listVerk = ['Romaner','Diktsamlingar', ' Noveller', 'Essäer']
+
+    for(let i = 0; i < 4; i++){
+        let textnode = document.createTextNode(listVerk[i]);
+        node.appendChild(textnode);
+        document.getElementById("menuVerk").appendChild(node).appendChild(document.createElement("br"));
+    }
+}
+
+function deleteChild() {
+    const e = document.getElementById("menuVerk");
+    
+    let child = e.firstChild; 
+    while (child) {
+        e.removeChild(child);
+        child = e.firstChild;
+    }
+}
+
