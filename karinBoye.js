952787736086c51fabd5fd067cd99b9821b27975
Ellen Gustafsson <0302ELGU@gapp.uddevalla.se>
2021-12-26 20:22:09 +0100

    fixes
    fixes after validation

--- karinBoye.js
+++ karinBoye.js
@@ -1,6 +1,19 @@
-//My plan here was to be able to hover over the underlinks that pop up when hovering over 'Verk' in the hamburger bar to make links to the diffrent pages for the works of karin boye
+let exports = {}; //has to be her in order for splide code to work
 
-function addLiVerk() { //function that shows underlinks while hovering over Verk in hamburger bar
+document.addEventListener( 'DOMContentLoaded', function() { //funtion for fade transition splidejs
+    let splide = new Splide( '.splide', {
+        type  : 'fade',
+        rewind: true,
+    } );
+      
+    splide.mount();
+} );
+
+
+//My plan here was to be able to hover over the undertitles that pop up when hovering over 'Verk' in the hamburger bar to make links to the diffrent pages for the works of karin boye, i did not manage to figure out how to do that in time for the deadline
+
+
+function addLiVerk() { //function that shows undertitle while hovering over Verk in hamburger bar
     const node = document.createElement("LI");
     node.className = "menuVerkChild";
     const listVerk = ['Romaner','Diktsamlingar', ' Noveller', 'Essäer']
@@ -12,7 +25,7 @@ function addLiVerk() { //function that shows underlinks while hovering over Verk
     }
 }
 
-function deleteChild() { //function for removing underlinks when not hovering over Verk in the hamburger sidebar
+function deleteChild() { //function for removing undertitle when not hovering over Verk in the hamburger sidebar
     const e = document.getElementById("menuVerk");
     
     let child = e.firstChild; 
@@ -21,4 +34,5 @@ function deleteChild() { //function for removing underlinks when not hovering ov
         child = e.firstChild;
     }
 }
+
 
